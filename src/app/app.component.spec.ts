9e15008e0fec4bcb7cdfb974a48d24e8a4208b41
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-04-28 23:31:05 +0300

    Rename to just Angular Excel

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,16 +28,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'Angular 2 Excel'`, async(() => {
+  it(`should have as title 'Angular Excel'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Angular 2 Excel');
+    expect(app.title).toEqual('Angular Excel');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Angular 2 Excel');
+    expect(compiled.querySelector('h1').textContent).toContain('Angular Excel');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Angular 2 Excel</h1>
+  template: `<h1>Angular Excel</h1>
              <excel></excel>`
 })
 export class AppComponent { }
