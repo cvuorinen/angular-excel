058f7b921a03a9046478c2fb7e8e535f01bb3dd1
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-04-29 08:08:35 +0300

    Fix existing autogenerated tests and write tests for excel component

--- src/app/excel/excel.component.spec.ts
+++ src/app/excel/excel.component.spec.ts
@@ -8,6 +8,7 @@ import { SpreadsheetService } from "app/spreadsheet.service";
 describe('ExcelComponent', () => {
   let component: ExcelComponent;
   let fixture: ComponentFixture<ExcelComponent>;
+  let element: HTMLElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -27,10 +28,24 @@ describe('ExcelComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ExcelComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have columns and rows`, async(() => {
+    expect(component.columns.length).toBeGreaterThan(0);
+    expect(component.rows.length).toBeGreaterThan(0);
+  }));
+
+  it('should render a table with columns and rows', async(() => {
+    expect(element.querySelector('table').rows.length).toEqual(component.rows.length + 1);
+  }));
+
+  it('should render a cell element in each table cell', async(() => {
+    expect(element.querySelectorAll('cell').length).toEqual(component.columns.length * component.rows.length);
+  }));
 });
