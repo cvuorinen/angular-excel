3f93bc66568e3eb834f73d2715311f7409ef8647
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-05-03 08:04:26 +0300

    Tests for SpreadsheetService

--- src/app/spreadsheet.service.spec.ts
+++ src/app/spreadsheet.service.spec.ts
@@ -1,15 +1,67 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, fakeAsync, inject, tick } from '@angular/core/testing';
+import { Subject } from "rxjs";
+import * as ngParser from "ng-parser";
 
 import { SpreadsheetService } from './spreadsheet.service';
 
 describe('SpreadsheetService', () => {
+  let service: SpreadsheetService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [SpreadsheetService]
     });
+    service = TestBed.get(SpreadsheetService);
   });
 
-  it('should ...', inject([SpreadsheetService], (service: SpreadsheetService) => {
-    expect(service).toBeTruthy();
+  it('should expose `update$` Subject', () => {
+    expect(service.update$ instanceof Subject).toBeTruthy();
+  });
+
+  describe('evaluate', () => {
+    const validData = [
+      { input: '1+1', result: 2 },
+      { input: '2*2', result: 4 },
+      { input: 'x+1', result: 1 },
+      { input: '1/0', result: Infinity },
+      { input: 'x*7', result: NaN },
+    ];
+    validData.map(data => {
+      it('should evaluate expression', () => {
+        expect(service.evaluate(data.input)).toEqual(data.result);
+      });
+    });
+
+    const invalidData = [
+      { input: '"foo' },
+      { input: 'throw x' },
+    ];
+    invalidData.map(data => {
+      it('should return the expression when parsing fails', () => {
+        expect(service.evaluate(data.input)).toEqual(data.input);
+      });
+    });
+  });
+
+  it('should evaluate with updated cell values', fakeAsync(() => {
+    const expression = 'A1+2';
+    service.update$.next({ id: 'A1', value: 2 });
+    tick();
+
+    expect(service.evaluate(expression)).toEqual(4);
+
+    service.update$.next({ id: 'A1', value: 7 });
+    tick();
+
+    expect(service.evaluate(expression)).toEqual(9);
+  }));
+
+  it('should evaluate sum function', fakeAsync(() => {
+    const expression = 'sum(A1,B1,4)';
+    service.update$.next({ id: 'A1', value: 2 });
+    service.update$.next({ id: 'B1', value: 3 });
+    tick();
+
+    expect(service.evaluate(expression)).toEqual(9);
   }));
 });
