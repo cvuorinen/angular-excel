6be4149b7b0ee0657133ba44fcc24cd1032055e4
Carl Vuorinen <carl.vuorinen@gmail.com>
2017-05-04 08:27:52 +0300

    E2E tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,80 @@
 import { AngularExcelPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('angular-excel App', () => {
   let page: AngularExcelPage;
 
   beforeEach(() => {
     page = new AngularExcelPage();
+    //page.addDelay();
   });
 
-  it('should display message saying app works', () => {
+  it('should display heading', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getHeadingText()).toEqual('Angular Excel');
+  });
+
+  it('should display a table of cells', () => {
+    page.navigateTo();
+    expect(page.getCellElements().count()).toBe(12);
+  });
+
+  it('should evaluate math expression and display result', () => {
+    const cellId = 'A1';
+    page.navigateTo();
+    page.setCellInput(cellId, '2+2');
+
+    expect(page.getCellOutput(cellId)).toBe('4');
+  });
+
+  it('should reference value from another cell', () => {
+    const cell1Id = 'A1';
+    const cell2Id = 'A2';
+    page.navigateTo();
+    page.setCellInput(cell1Id, '3');
+    page.setCellInput(cell2Id, cell1Id);
+
+    expect(page.getCellOutput(cell2Id)).toBe('3');
+  });
+
+  it('should evaluate function call', () => {
+    const cell1Id = 'A1';
+    const cell2Id = 'A2';
+    page.navigateTo();
+    page.setCellInput(cell1Id, '3');
+    page.setCellInput(cell2Id, `sum(${cell1Id},2)`);
+
+    expect(page.getCellOutput(cell2Id)).toBe('5');
+  });
+
+  it('should re-evaluate expression when referenced cell changes', () => {
+    const cell1Id = 'A1';
+    const cell2Id = 'A2';
+    page.navigateTo();
+    //browser.pause();
+    page.setCellInput(cell1Id, '2');
+    page.setCellInput(cell2Id, `${cell1Id}*2`);
+
+    expect(page.getCellOutput(cell2Id)).toBe('4');
+
+    page.setCellInput(cell1Id, '7');
+
+    expect(page.getCellOutput(cell2Id)).toBe('14');
+  });
+
+  it('should cascade re-evaluation throught multiple cell changes', () => {
+    const cell1Id = 'A1';
+    const cell2Id = 'A2';
+    const cell3Id = 'A3';
+    page.navigateTo();
+    page.setCellInput(cell1Id, '2');
+    page.setCellInput(cell2Id, `${cell1Id}+2`);
+    page.setCellInput(cell3Id, `sum(${cell1Id},${cell2Id},2)`);
+
+    expect(page.getCellOutput(cell3Id)).toBe('8');
+
+    page.setCellInput(cell1Id, '7');
+
+    expect(page.getCellOutput(cell3Id)).toBe('18');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,37 @@
-import { browser, element, by } from 'protractor';
+import { protractor, browser, element, by, Key } from 'protractor';
 
 export class AngularExcelPage {
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
+  addDelay(delay: number = 10) {
+    var origFn = browser.driver.controlFlow().execute;
+    browser.driver.controlFlow().execute = (...args) => {
+      // queue wait to adjust the execution speed
+      origFn.call(browser.driver.controlFlow(), () => {
+        return protractor.promise.delayed(delay);
+      });
+
+      return origFn.apply(browser.driver.controlFlow(), args);
+    };
+  }
+
+  getHeadingText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+  getCellElements() {
+    return element.all(by.css('cell'));
+  }
+
+  setCellInput(cellId: string, text: string) {
+    const input = element(by.css(`cell[id="${cellId}"] input`))
+    return input.clear()
+      .then(() => input.sendKeys(text + Key.TAB)); // add tab to move focus out of the input
+  }
+
+  getCellOutput(cellId: string) {
+    return element(by.css(`cell[id="${cellId}"] .output`)).getText();
   }
 }
